9e2ff89d6a17fc92e7b77965f72fefe994e03790
Marc Biegota <marc.biegota@web.de>
2025-01-19 16:44:47 +0100

    introduce IThing

--- src/Heretic.Roguelike/Things/ICreature.cs
+++ src/Heretic.Roguelike/Things/ICreature.cs
@@ -1,13 +1,10 @@
 ﻿using System.Collections.Generic;
-using Heretic.Roguelike.ArtificialIntelligence.Movements;
 using Heretic.Roguelike.Dices;
-using Heretic.Roguelike.Numerics;
 
 namespace Heretic.Roguelike.Things;
 
-public interface ICreature<T>
+public interface ICreature<T> : IThing<T>
 {
-    IMotionController<T> MotionController { get; set; }
     int Experience { get; set; }
     byte ExperienceLevel { get; set; }
     ushort HitPoints { get; set; }
@@ -16,8 +13,4 @@ public interface ICreature<T>
     ushort MaxStrength { get; set; }
     sbyte AmourClass { get; set; }
     IList<DiceThrow> Damage { get; init; }
-    T Icon { get; init; }
-    Vector ActualPosition { get; }
-    void Translate(Vector offset);
-    void Translate();
 }
---
+++ src/Heretic.Roguelike/Things/IThing.cs
@@ -0,0 +1,13 @@
+﻿using Heretic.Roguelike.ArtificialIntelligence.Movements;
+using Heretic.Roguelike.Numerics;
+
+namespace Heretic.Roguelike.Things;
+
+public interface IThing<T>
+{
+    IMotionController<T> MotionController { get; set; }
+    T Icon { get; init; }
+    Vector ActualPosition { get; }
+    void Translate(Vector offset);
+    void Translate();
+}
